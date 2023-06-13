0aa369ae6fd5d0153710b2e9fff6a48ebb086e99
Janet Suryadi <janet@suryadi.id.au>
2023-06-13 13:46:02 +1000

    Tinkered with JS and tried to get the project hover working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,9 +4,10 @@
 
 /*----------  Variable Declaration  ----------*/
 
-
+let projectsList = document.getElementsByClassName("projects__project-list");
+let projectsListItems = [];
+console.log(projectsList);
 
 /*----------  Script assignment  ----------*/
-
 
 
